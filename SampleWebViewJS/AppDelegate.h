35305d6936798702357d367d005b8be9894fa925
ei17ringo <ei17ringo@gmail.com>
2015-02-16 14:44:18 +0800

    Initial Commit

---
+++ SampleWebViewJS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleWebViewJS
+//
+//  Created by Eriko Ichinohe on 2015/02/16.
+//  Copyright (c) 2015年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleWebViewJS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleWebViewJS
+//
+//  Created by Eriko Ichinohe on 2015/02/16.
+//  Copyright (c) 2015年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
